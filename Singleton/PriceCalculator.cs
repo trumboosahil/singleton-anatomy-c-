e18cc656913b68d53b458ad734942c3dda7ee49a
Shafi <trumboo@theahoyapp.com>
2022-03-08 08:44:54 +0400

    first commit

---
+++ Singleton/PriceCalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class PriceCalculator
+    {
+        private static readonly Lazy<PriceCalculator> lazy = new Lazy<PriceCalculator>(() => new PriceCalculator());
+        public static PriceCalculator Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+        static int sharedtotal = 0;
+        public  int CalculatePrice(int unit, int unitprice,int quantity)
+        {
+            int total = 0;
+            for (int i = 0; i < quantity; i++)
+            {
+                total += unit * unitprice;
+                Thread.Sleep(100);
+            }
+            return total;
+        }
+
+        public  int CalculatePriceShare(int unit, int unitprice,int quantity)
+        {
+            Console.WriteLine("new");
+            for (int i = 0; i < quantity; i++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine(thread.ManagedThreadId);
+                sharedtotal += unit * unitprice;
+                Thread.Sleep(200);
+            }
+            return sharedtotal;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[3];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(sum));
+            }
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Start();
+            }
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
+           
+        }
+        static void sum()
+        {
+            //In case of local variable
+            //PriceCalculator priceCalculator = PriceCalculator.Instance;
+            //Console.WriteLine("Hello World!");
+            //int total = priceCalculator.CalculatePrice(100, 1,10);
+            //Console.WriteLine(total.ToString());
+
+            //In case of share variable
+            PriceCalculator priceCalculator = PriceCalculator.Instance;
+            int totalShare = priceCalculator.CalculatePriceShare(100, 1, 10);
+            Console.WriteLine(totalShare.ToString());
+            Console.ReadLine();
+        }
+
+    }
+}
